eb3867047b1e1006a03710c2f4f818d69d8beb6b
Emjay Factor <emjayfactor@gmail.com>
2024-12-12 13:25:57 +0800

    Update script.py

--- script.py
+++ script.py
@@ -5,5 +5,5 @@ with open('README.md', 'r') as file: # the readme is our target file
 content = content.replace('unli-contribute', '###REPLACE###').replace('Unli-contribute', 'unli-contribute').replace('###REPLACE###', 'Unli-contribute', 1) # You can use any word that is in your target file
 
 
-with open('readme.md', 'w') as file:
+with open('README.md', 'w') as file:
     file.write(content) # This write the replaced word. 
