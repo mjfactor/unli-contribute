37c1e6286b55da8a1dd05dfbfafb055d4a68f346
Emjay Factor <emjayfactor@gmail.com>
2024-12-12 13:13:27 +0800

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ with open('README.md', 'r') as file: # the readme is our target file
     content = file.read()
 
 
-content = content.replace('Dynamic Programming', '###REPLACE###').replace('Docker', 'Dynamic Programming').replace('###REPLACE###', 'Docker', 1) # You can use any word that is in your target file
+content = content.replace('unli-contribute', '###REPLACE###').replace('Unli-contribute', 'unli-contribute').replace('###REPLACE###', 'Unli-contribute') # You can use any word that is in your target file
 
 
 with open('readme.md', 'w') as file:
