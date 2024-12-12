9cc614c7a6f6e7919fce5efa30c03f1615cccbe7
Emjay Factor <emjayfactor@gmail.com>
2024-12-12 12:36:42 +0800

    Create script.py

---
+++ script.py
@@ -0,0 +1,9 @@
+with open('readme.md', 'r') as file: # the readme is our target file
+    content = file.read()
+
+
+content = content.replace('Dynamic Programming', '###REPLACE###').replace('Docker', 'Dynamic Programming').replace('###REPLACE###', 'Docker', 1) # You can use any word that is in your target file
+
+
+with open('readme.md', 'w') as file:
+    file.write(content) # This write the replaced word. 
